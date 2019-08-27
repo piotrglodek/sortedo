6c7d8928cdc57032e8a25554d1b9eac77c027e06
unknown <glodekpiotr@wp.pl>
2019-08-27 15:00:45 +0200

    added products data

---
+++ app/data.js
@@ -0,0 +1,158 @@
+// fruit, grains, meat, vegetable, dairy
+const data = [
+ {
+  title: 'Grapes',
+  type: 'fruit',
+  emoji: '🍇'
+ },
+ {
+  title: 'Melon',
+  type: 'fruit',
+  emoji: '🍈'
+ },
+ {
+  title: 'Watermelon',
+  type: 'fruit',
+  emoji: '🍉'
+ },
+ {
+  title: 'Tangerine',
+  type: 'fruit',
+  emoji: '🍊'
+ },
+ {
+  title: 'Lemon',
+  type: 'fruit',
+  emoji: '🍋'
+ },
+ {
+  title: 'Banana',
+  type: 'fruit',
+  emoji: '🍌'
+ },
+ {
+  title: 'Pineapple',
+  type: 'fruit',
+  emoji: '🍍'
+ },
+ {
+  title: 'Mango',
+  type: 'fruit',
+  emoji: '🥭'
+ },
+ {
+  title: 'Red Apple',
+  type: 'fruit',
+  emoji: '🍎'
+ },
+ {
+  title: 'Green Apple',
+  type: 'fruit',
+  emoji: '🍏'
+ },
+ {
+  title: 'Pear',
+  type: 'fruit',
+  emoji: '🍐'
+ },
+ {
+  title: 'Peach',
+  type: 'fruit',
+  emoji: '🍑'
+ },
+ {
+  title: 'Cherries',
+  type: 'fruit',
+  emoji: '🍒'
+ },
+ {
+  title: 'Strawberry',
+  type: 'fruit',
+  emoji: '🍓'
+ },
+ {
+  title: 'Kiwi',
+  type: 'fruit',
+  emoji: '🥝'
+ },
+ {
+  title: 'Tomato',
+  type: 'vegetable',
+  emoji: '🍅'
+ },
+ {
+  title: 'Coconut',
+  type: 'fruit',
+  emoji: '🥥'
+ },
+ {
+  title: 'Avocado',
+  type: 'fruit',
+  emoji: '🥑'
+ },
+ {
+  title: 'Potato',
+  type: 'vegetable',
+  emoji: '🥔'
+ },
+ {
+  title: 'Carrot',
+  type: 'vegetable',
+  emoji: '🥕'
+ },
+ {
+  title: 'Corn',
+  type: 'grains',
+  emoji: '🌽'
+ },
+ {
+  title: 'Hot Pepper',
+  type: 'vegetable',
+  emoji: '🌶️'
+ },
+ {
+  title: 'Peanuts',
+  type: 'fruit',
+  emoji: '🥜'
+ },
+ {
+  title: 'Bread',
+  type: 'grains',
+  emoji: '🍞'
+ },
+ {
+  title: 'Croissant',
+  type: 'grains',
+  emoji: '🥐'
+ },
+ {
+  title: 'Baguette',
+  type: 'grains',
+  emoji: '🥖'
+ },
+ {
+  title: 'Pretzel',
+  type: 'grains',
+  emoji: '🥨'
+ },
+ {
+  title: 'Bagel',
+  type: 'grains',
+  emoji: '🥯'
+ },
+ {
+  title: 'Cheese',
+  type: 'dairy',
+  emoji: '🧀'
+ },
+ {
+  title: 'Poultry Leg',
+  type: 'meat',
+  emoji: '🍗'
+ },
+ {
+  title: 'Bacon',
+  type: 'meat',
+  emoji: '🥓'
+ }
+];
